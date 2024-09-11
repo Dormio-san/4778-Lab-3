2196039f79c7a02a0ef217b0aef327d47a1c3b40
NicolasSpohn <149342232+NicolasSpohn@users.noreply.github.com>
2024-09-11 14:55:46 -0400

    EnemyMovement script

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    Vector3 Force;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 relativePos = (target.position + new Vector3(0, 0, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 3 * Time.deltaTime);
     }
 }
