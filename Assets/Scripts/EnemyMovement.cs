5c67205e8ea858da99adc3c58a0a5a6de6e5e644
Dormio-san <liam.lovett06@gmail.com>
2024-09-12 16:39:41 -0400

    Tweak enemy rotation to better follow player.
    Looking pretty good, but still need to make sure enemies face the player at all times.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    private float rotationSpeed = 70f;
+    private Transform player;
+    private float rotationSpeed = 60f;
     
     // The min and max distances that the enemies are allowed to be from the player.
-    private float minDistance = 6.5f;
-    private float maxDistance = 50f;
+    private float minDistance = 6f;
 
-    // The speed at which the enemy moves away from the player when they are too close.
-    private float moveAwaySpeed = 2f;
+    // The speed at which the enemy moves.
+    private float moveSpeed;
+    private float minMoveSpeed = 2f;
+    private float maxMoveSpeed = 3.5f;
 
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        // Randomizer the enemy moveSpeed
+        moveSpeed = Random.Range(minMoveSpeed, maxMoveSpeed);
+    }
     private void Update()
     {
         // Distance from the enemy to the player.
@@ -22,19 +31,23 @@ public class EnemyMovement : MonoBehaviour
         // If the enemy is too close to the player, move the enemy away at an increasing speed based on the proximity to the player.
         if (distance < minDistance)
         {
-            float moveSpeed = (minDistance - distance) * moveAwaySpeed;
+            // Speed that the enemy will use to move away from the player when they are too close that is based on their proximity to the player.
+            float moveAwaySpeed = (minDistance - distance) * moveSpeed;
 
-            Vector3 directionAway = (transform.position - player.position);
-            transform.position += directionAway * moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, -moveAwaySpeed * Time.deltaTime);
         }
         
         // If the enemy is too far from the player, get closer to the player.
-        else if (distance > maxDistance)
+        else if (distance > minDistance)
         {
-            Vector3 directionToPlayer = (transform.position - player.position);
-            transform.position = directionToPlayer * (distance - maxDistance) * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
         }
 
         transform.RotateAround(player.position, Vector3.forward, rotationSpeed * Time.deltaTime);
     }
+
+    //private void LookAtPlayer()
+    //{
+    //    Vector3 lookDirection = transform.InverseTransformPoint(player.transform.position);
+    //}
 }
